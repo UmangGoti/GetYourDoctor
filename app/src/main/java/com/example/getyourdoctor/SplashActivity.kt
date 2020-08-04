416d5995a204f3391d804c1f6289e4e4ecaebf66
DevamDesai <devamdesai12@gmail.com>
2020-08-04 16:06:40 +0530

    Splash Screen added

--- app/src/main/java/com/example/getyourdoctor/SplashActivity.kt
+++ app/src/main/java/com/example/getyourdoctor/SplashActivity.kt
@@ -9,8 +9,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
-//       val_handler = Handler()
+        
         Handler().postDelayed(
             {
                 val i = Intent(this, HomeActivity::class.java)
