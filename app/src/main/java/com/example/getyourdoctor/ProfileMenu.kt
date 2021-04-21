db0f06abd00f93b46a008a0a47f530cae01bcc14
Umang Goti <umanggoti1@gmail.com>
2021-04-21 21:07:07 +0530

    Particular medicalinfo activity added,Edit Profile option added,UI Updated

--- app/src/main/java/com/example/getyourdoctor/ProfileMenu.kt
+++ app/src/main/java/com/example/getyourdoctor/ProfileMenu.kt
@@ -6,15 +6,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.*
+import com.google.firebase.database.*import kotlinx.android.synthetic.main.activity_hospital_info.*
+import kotlinx.android.synthetic.main.fragment_profile_menu.*
 import kotlinx.android.synthetic.main.fragment_profile_menu.view.*
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 class ProfileMenu : Fragment() {
 
@@ -24,11 +27,9 @@ class ProfileMenu : Fragment() {
     val fragmentTransaction:FragmentTransaction? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activity?.onBackPressedDispatcher?.addCallback(this,object : OnBackPressedCallback(true){
+        activity?.onBackPressedDispatcher?.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                val fragmentTransaction = fragmentManager?.beginTransaction()
-                fragmentTransaction?.replace(R.id.homeframeLayout,ProfileMenu())
-                fragmentTransaction?.commit()
+                System.exit(0)
             }
         })
     }
@@ -46,40 +47,51 @@ class ProfileMenu : Fragment() {
         val fragmentTransaction = fragmentManager?.beginTransaction()
 
         if(currentuser == null){
-            fragmentTransaction?.replace(R.id.homeframeLayout,Login())
+            fragmentTransaction?.replace(R.id.homeframeLayout, Login())
             fragmentTransaction?.remove(this)
             fragmentTransaction?.commit()
         }else{
             loadprofile(view)
         }
-        view.logoutBtn.setOnClickListener {
+
+        view.logoutIcon.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
-            fragmentTransaction?.replace(R.id.homeframeLayout,Login())
+            fragmentTransaction?.replace(R.id.homeframeLayout, Login())
             fragmentTransaction?.remove(this)
             fragmentTransaction?.commit()
         }
+
+        view.editIcon.setOnClickListener {
+            EditInformation(view)
+        }
+
         return view
     }
+
     private fun loadprofile(view: View){
         val user = auth.currentUser
         val userreference = databaseReference?.child(user?.uid!!)
         userreference?.addValueEventListener(object : ValueEventListener {
             @RequiresApi(Build.VERSION_CODES.O)
             override fun onDataChange(snapshot: DataSnapshot) {
+
                 view.userName.text = snapshot.child("Name").value.toString()
+                view.userName.setSingleLine()
+                view.userName.isSelected = true
+
                 var formatter = SimpleDateFormat("dd/MM/yyyy")
-                var date=formatter.parse(snapshot.child("Dob").value.toString())
+                var date = formatter.parse(snapshot.child("Dob").value.toString())
                 var dob = Calendar.getInstance()
-                dob.time = date
+//                dob.time = date
                 val dobYear = dob.get(Calendar.YEAR)
                 val dobMonth = dob.get(Calendar.MONTH)
                 val dobDay = dob.get(Calendar.DAY_OF_MONTH)
                 var age = getAge(dobYear, dobMonth, dobDay)
-                view.userAge.text = "Current Age : " + age+" ("+snapshot.child("Dob").value.toString()+")"
-                view.userContact.text= "Contact     : "+snapshot.child("Contact").value.toString()
-                view.userEmail.text= "Email       : "+snapshot.child("Email").value.toString()
+                view.userAge.text = age + " (" + snapshot.child("Dob").value.toString() + ")"
 
+                view.userContact.text = snapshot.child("Contact").value.toString()
 
+                view.userEmail.text = snapshot.child("Email").value.toString()
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -87,6 +99,7 @@ class ProfileMenu : Fragment() {
             }
         })
     }
+
     private fun getAge(year: Int, month: Int, day: Int): String {
         val dob = Calendar.getInstance()
         val today = Calendar.getInstance()
@@ -97,5 +110,120 @@ class ProfileMenu : Fragment() {
         }
         val ageInt = age
         return ageInt.toString()
+    }
+
+    private fun EditInformation(view: View){
+        view.userName.visibility = View.GONE
+        view.editIcon.visibility =View.GONE
+        view.ageIcon.visibility = View.GONE
+        view.userAge.visibility = View.GONE
+        view.callIcon.visibility = View.GONE
+        view.userContact.visibility = View.GONE
+        view.emailIcon.visibility = View.GONE
+        view.userEmail.visibility = View.GONE
+        view.transectionBtn.visibility = View.GONE
+        view.view1.visibility = View.GONE
+        view.view2.visibility = View.GONE
+        view.view3.visibility = View.GONE
+        view.view4.visibility = View.GONE
+        view.view5.visibility = View.GONE
+
+        view.userNameIcon.visibility = View.VISIBLE
+        view.userNameEt.visibility = View.VISIBLE
+        view.userDobIcon.visibility = View.VISIBLE
+        view.userDobEt.visibility = View.VISIBLE
+        view.userCallIcon.visibility = View.VISIBLE
+        view.userContactEt.visibility = View.VISIBLE
+        view.updateBtn.visibility = View.VISIBLE
+
+        val user = auth.currentUser
+        val userreference = databaseReference?.child(user?.uid!!)
+        userreference?.addValueEventListener(object : ValueEventListener {
+            @RequiresApi(Build.VERSION_CODES.O)
+            override fun onDataChange(snapshot: DataSnapshot) {
+                view.userNameEt.setText(snapshot.child("Name").value.toString())
+                view.userDobEt.setText(snapshot.child("Dob").value.toString())
+                view.userContactEt.setText(snapshot.child("Contact").value.toString())
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+        view.updateBtn.setOnClickListener {
+            val user = auth.currentUser
+            val userreference = databaseReference?.child(user?.uid!!)
+            var flag:Int = 0
+            userreference?.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val key = snapshot.key
+                    if (key != null && (view.userNameEt.text.toString()).isNotEmpty() && (view.userContactEt.text.toString()).isNotEmpty() && (view.userDobEt.text.toString()).isNotEmpty()) {
+                        userreference.child("Name").setValue(view.userNameEt.text.toString())
+                        userreference.child("Dob").setValue(view.userDobEt.text.toString())
+                        userreference.child("Contact").setValue(view.userContactEt.text.toString())
+                        flag = 1
+                    } else {
+                        flag = 0
+                        Toast.makeText(context, "Enter Valid Value", Toast.LENGTH_SHORT).show()
+                    }
+
+                    if (flag == 1) {
+                        view.userNameIcon.visibility = View.GONE
+                        view.userNameEt.visibility = View.GONE
+                        view.userDobIcon.visibility = View.GONE
+                        view.userDobEt.visibility = View.GONE
+                        view.userCallIcon.visibility = View.GONE
+                        view.userContactEt.visibility = View.GONE
+                        view.updateBtn.visibility = View.GONE
+
+
+                        view.userName.visibility = View.VISIBLE
+                        view.editIcon.visibility = View.VISIBLE
+                        view.ageIcon.visibility = View.VISIBLE
+                        view.userAge.visibility = View.VISIBLE
+                        view.callIcon.visibility = View.VISIBLE
+                        view.userContact.visibility = View.VISIBLE
+                        view.emailIcon.visibility = View.VISIBLE
+                        view.userEmail.visibility = View.VISIBLE
+                        view.transectionBtn.visibility = View.VISIBLE
+                        view.view1.visibility = View.VISIBLE
+                        view.view2.visibility = View.VISIBLE
+                        view.view3.visibility = View.VISIBLE
+                        view.view4.visibility = View.VISIBLE
+                        view.view5.visibility = View.VISIBLE
+                    } else {
+                        view.userName.visibility = View.GONE
+                        view.editIcon.visibility = View.GONE
+                        view.ageIcon.visibility = View.GONE
+                        view.userAge.visibility = View.GONE
+                        view.callIcon.visibility = View.GONE
+                        view.userContact.visibility = View.GONE
+                        view.emailIcon.visibility = View.GONE
+                        view.userEmail.visibility = View.GONE
+                        view.transectionBtn.visibility = View.GONE
+                        view.view1.visibility = View.GONE
+                        view.view2.visibility = View.GONE
+                        view.view3.visibility = View.GONE
+                        view.view4.visibility = View.GONE
+                        view.view5.visibility = View.GONE
+
+
+                        view.userNameIcon.visibility = View.VISIBLE
+                        view.userNameEt.visibility = View.VISIBLE
+                        view.userDobIcon.visibility = View.VISIBLE
+                        view.userDobEt.visibility = View.VISIBLE
+                        view.userCallIcon.visibility = View.VISIBLE
+                        view.userContactEt.visibility = View.VISIBLE
+                        view.updateBtn.visibility = View.VISIBLE
+                    }
+
+                }
+
+                override fun onCancelled(p0: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+        }
     }
 }
--- app/src/main/java/com/example/getyourdoctor/adapter/MedicalAdapter.kt
+++ app/src/main/java/com/example/getyourdoctor/adapter/MedicalAdapter.kt
@@ -1,14 +1,18 @@
 package com.example.getyourdoctor.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.getyourdoctor.HospitalInfo
+import com.example.getyourdoctor.MedicalInfo
 import com.example.getyourdoctor.R
 import com.example.getyourdoctor.dataclass.MedicalData
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MedicalAdapter(
@@ -33,26 +37,45 @@ class MedicalAdapter(
         override fun onBindViewHolder(holder: MedicalViewholderClass, position: Int) {
             val mediaclViewholderClass = holder
             val listPosition = medicalDataList!![position]
-            mediaclViewholderClass.mName.text = listPosition.getmName()
-            if (listPosition.getmTime().equals("Open", ignoreCase = true)) {
-                listPosition.getmTime()?.toUpperCase(locale = Locale.ROOT)
-                    .also { mediaclViewholderClass.mTime.text = it }
-                mediaclViewholderClass.mTime.setTextColor(Color.GREEN)
-            } else if (listPosition.getmTime().equals("Close", ignoreCase = true)) {
-                listPosition.getmTime()?.toUpperCase(locale = Locale.ROOT)
-                    .also { mediaclViewholderClass.mTime.text = it }
-                mediaclViewholderClass.mTime.setTextColor(Color.RED)
+
+            mediaclViewholderClass.medicalName.text = listPosition.getmName()
+            mediaclViewholderClass.medicalName.setSingleLine()
+            mediaclViewholderClass.medicalName.isSingleLine = true
+
+            val cal = Calendar.getInstance()
+            val sdfHour = SimpleDateFormat("HH:mm")
+            val hour: String = sdfHour.format(cal.time)
+
+            if (listPosition.getmTime1()?.compareTo(hour)!! < 0 && (listPosition.getmTime2()?.compareTo(hour)!! > 0)){
+                mediaclViewholderClass.medicalTime.text = "Open"
+                mediaclViewholderClass.medicalTime.setTextColor(Color.GREEN)
+            }else{
+                mediaclViewholderClass.medicalTime.text = "Close"
+                mediaclViewholderClass.medicalTime.setTextColor(Color.RED)
+            }
+
+            holder.itemView.setOnClickListener {
+                var intent = Intent(context, MedicalInfo::class.java)
+                intent.putExtra("MclName", listPosition.getmName())
+                intent.putExtra("MclAddress", listPosition.getmAdd())
+                intent.putExtra("MclTime",listPosition.getmTime1()+" to "+listPosition.getmTime2())
+                intent.putExtra("MclContact",listPosition.getmPhone().toString())
+                intent.putExtra("MclName1", listPosition.getmName())
+                intent.putExtra("MclLat1", listPosition.getmLat())
+                intent.putExtra("MclLong1", listPosition.getmLong())
+
+                context.startActivity(intent)
             }
         }
 
         inner class MedicalViewholderClass(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            var mName: TextView
-            var mTime: TextView
+            var medicalName: TextView
+            var medicalTime: TextView
             init {
-                mName = itemView.findViewById(R.id.medicalName)
-                mName.setSingleLine()
-                mName.isSelected = true
-                mTime = itemView.findViewById(R.id.medicalTime)
+                medicalName = itemView.findViewById(R.id.mName)
+                medicalName.setSingleLine()
+                medicalName.isSelected = true
+                medicalTime = itemView.findViewById(R.id.mTime)
             }
         }
         init {
